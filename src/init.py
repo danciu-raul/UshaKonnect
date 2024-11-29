9fb5e9bf42822b2a717270b4f7337e0556ac3083
lipiec-kacper <lipiec2003kacper@gmail.com>
2024-11-29 03:10:43 +0100

    home page desing

--- src/init.py
+++ src/init.py
@@ -3,8 +3,12 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def home():
+def main():
     return render_template('pages/auth.html')
+
+@app.route('/home')
+def home():
+    return render_template('pages/home.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
