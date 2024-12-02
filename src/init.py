96bf55bd0051438cbc7bf619c8050cedf868f466
lipiec-kacper <lipiec2003kacper@gmail.com>
2024-12-03 00:40:38 +0100

    creation of database schema

--- src/init.py
+++ src/init.py
@@ -1,22 +1,46 @@
+import os
 from flask import Flask, render_template
+from src.instance import db
 
-app = Flask(__name__)
 
-@app.route('/')
-def main():
-    return render_template('pages/auth.html')
+def create_app(test_config=None):
+    # Create and configure the app
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_mapping(
+        SECRET_KEY='dev',
+        DATABASE=os.path.join(app.instance_path, 'db.sqlite')
+    )
 
-@app.route('/home')
-def home():
-    return render_template('pages/home.html')
+    # Ensure the instance folder exists
+    os.makedirs(app.instance_path, exist_ok=True)
 
-@app.route('/admin')
-def admin():
-    return render_template('pages/admin.html')
+    # Initialize database
+    db.init_app(app)
+    if not os.path.exists(app.config['DATABASE']):
+        with app.app_context():
+            db.init_db()
+            print("Database initialized.")
 
-@app.route('/notActivated')
-def notActivated():
-    return render_template('pages/notActivated.html')
+    # Define routes
+    @app.route('/')
+    def main():
+        return render_template('pages/auth.html')
 
-if __name__ == '__main__':
+    @app.route('/home')
+    def home():
+        return render_template('pages/home.html')
+
+    @app.route('/admin')
+    def admin():
+        return render_template('pages/admin.html')
+
+    @app.route('/notActivated')
+    def notActivated():
+        return render_template('pages/notActivated.html')
+
+    return app
+
+
+if __name__ == "__main__":
+    app = create_app()
     app.run(debug=True)
